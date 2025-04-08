34240ff1d6664b7160d780dffbf28437a56f833c
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2025-04-09 00:58:19 +0300

    add readme

--- Task_2_deque.py
+++ Task_2_deque.py
@@ -1,12 +1,6 @@
-# Необхідно розробити функцію, яка приймає рядок як вхідний параметр, додає всі його символи 
-# до двосторонньої черги (deque з модуля collections в Python), 
-# а потім порівнює символи з обох кінців черги, щоб визначити, чи є рядок паліндромом. 
-# Програма повинна правильно враховувати як рядки з парною, так і з непарною кількістю символів, 
-# а також бути нечутливою до регістру та пробілів.
-
 from collections import deque
 
-def check_palindrom(input_string):
+def check_palindrome(input_string):
     cleaned = ''.join(c.lower() for c in input_string if c.isalnum())
     
     cleaned_string = deque(cleaned)
@@ -18,8 +12,8 @@ def check_palindrom(input_string):
     return True
 
 
-print(check_palindrom("Was it a car or a cat I saw?"))     
-print(check_palindrom("A man a plan a canal Panama"))
-print(check_palindrom("Hello"))  
-print(check_palindrom("123454321"))
-print(check_palindrom("No l em  on, no mel  on"))               # True
+print(check_palindrome("Was it a car or a cat I saw?"))     
+print(check_palindrome("A man a plan a canal Panama"))
+print(check_palindrome("Hello"))  
+print(check_palindrome("123454321"))
+print(check_palindrome("No l em  on, no mel  on"))               # True
